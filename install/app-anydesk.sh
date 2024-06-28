292c9231c34183796a10db42a158f7939e37da15
David <davidjr82@gmail.com>
2024-06-28 13:09:21 +0200

    Update app-anydesk.sh

--- install/app-anydesk.sh
+++ install/app-anydesk.sh
@@ -1,5 +1,5 @@
 cd /tmp
-wget -O code.deb 'https://anydesk.com/es/downloads/thank-you?dv=deb_64'
+wget -O code.deb 'https://download.anydesk.com/linux/anydesk_6.3.2-1_amd64.deb'
 sudo apt install -y ./code.deb
 rm code.deb
 cd -
