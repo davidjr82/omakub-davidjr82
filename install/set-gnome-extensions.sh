715907527bd557ca0ab0bf682030bb8dce4b675d
David Jiménez <djimenez@edatta.com>
2024-06-28 20:05:27 +0200

    wip

--- install/set-gnome-extensions.sh
+++ install/set-gnome-extensions.sh
@@ -2,10 +2,10 @@ sudo apt install -y gnome-shell-extension-manager pipx
 pipx install --force gnome-extensions-cli --system-site-packages
 
 # Turn off default Ubuntu extensions
-# gnome-extensions disable tiling-assistant@ubuntu.com
-# gnome-extensions disable ubuntu-appindicators@ubuntu.com
-# gnome-extensions disable ubuntu-dock@ubuntu.com
-# gnome-extensions disable ding@rastersoft.com
+gnome-extensions disable tiling-assistant@ubuntu.com
+gnome-extensions disable ubuntu-appindicators@ubuntu.com
+gnome-extensions disable ubuntu-dock@ubuntu.com
+gnome-extensions disable ding@rastersoft.com
 
 # Install new extensions
 gext install just-perfection-desktop@just-perfection
