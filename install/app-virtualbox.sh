64ad04e6d0587b29004e6911f7503817023bc5ac
David <davidjr82@gmail.com>
2024-06-28 15:57:46 +0200

    Update app-virtualbox.sh

--- install/app-virtualbox.sh
+++ install/app-virtualbox.sh
@@ -1,3 +1,3 @@
 sudo apt install linux-headers-$(uname -r) dkms
-sudo apt install virtualbox virtualbox-guest-additions-iso virtualbox-guest-utils
+sudo apt install virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso virtualbox-guest-utils
 sudo usermod -a -G vboxusers $USER
