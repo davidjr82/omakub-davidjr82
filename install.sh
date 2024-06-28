bc641976a9818fb11b256553341fd8ace683e1ab
David Jiménez <djimenez@edatta.com>
2024-06-28 17:06:41 +0200

    wip

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ set_font() {
   if ! $(fc-list | grep -i "$font_name" > /dev/null); then
     cd /tmp
     wget -O "$file_name.zip" "$url"
-    unzip "$file_name.zip" -d "$file_name"
+    unzip -o "$file_name.zip" -d "$file_name"
     cp "$file_name"/*."$file_type" ~/.local/share/fonts
     rm -rf "$file_name.zip" "$file_name"
     fc-cache
