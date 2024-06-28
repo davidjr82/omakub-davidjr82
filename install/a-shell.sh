f9f23139bf08389c547b2253b969fc5741fe2192
David Jiménez <djimenez@edatta.com>
2024-06-28 16:31:35 +0200

    wip

--- install/a-shell.sh
+++ install/a-shell.sh
@@ -1,6 +1,6 @@
 [ -f "~/.bashrc" ] && mv ~/.bashrc ~/.bashrc.bak
-cp ~/.local/share/omakub-davejr82/defaults/bash/rc ~/.bashrc
-source ~/.local/share/omakub-davejr82/defaults/bash/shell
+cp ~/.local/share/omakub-davidjr82/defaults/bash/rc ~/.bashrc
+source ~/.local/share/omakub-davidjr82/defaults/bash/shell
 
 [ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
-cp ~/.local/share/omakub-davejr82/defaults/bash/inputrc ~/.inputrc
+cp ~/.local/share/omakub-davidjr82/defaults/bash/inputrc ~/.inputrc
