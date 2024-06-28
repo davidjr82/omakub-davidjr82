9dd665c4680f71f36cb2e5e1be46da728ec3c87d
David <davidjr82@gmail.com>
2024-06-28 15:44:16 +0200

    Update and rename app-mysql.sh to app-database.sh

---
+++ install/app-database.sh
@@ -0,0 +1,2 @@
+sudo apt install mysql-client mysql-server redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev memcached
+sudo systemctl enable --now mysql
--- install/app-mysql.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install mysql-client mysql-server
-sudo systemctl enable --now mysql
