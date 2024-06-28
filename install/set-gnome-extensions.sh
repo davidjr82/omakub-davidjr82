ee200175acae4e1a0584326d814d2002467037b3
David Jiménez <djimenez@edatta.com>
2024-06-28 20:06:10 +0200

    wip

--- install/set-gnome-extensions.sh
+++ install/set-gnome-extensions.sh
@@ -7,6 +7,9 @@ gnome-extensions disable ubuntu-appindicators@ubuntu.com
 gnome-extensions disable ubuntu-dock@ubuntu.com
 gnome-extensions disable ding@rastersoft.com
 
+# Pause to assure user is ready to accept confirmations
+gum confirm "To install Gnome extensions, you need to accept confirmations. Are you ready?"
+
 # Install new extensions
 gext install just-perfection-desktop@just-perfection
 gext install blur-my-shell@aunetx
