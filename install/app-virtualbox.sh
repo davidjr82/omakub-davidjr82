e40ad0d23911258e98467378eb79378f5ea232f9
David <davidjr82@gmail.com>
2024-06-30 09:28:38 +0200

    Update app-virtualbox.sh

--- install/app-virtualbox.sh
+++ install/app-virtualbox.sh
@@ -1,3 +1,5 @@
+# See https://ubuntu.com/tutorials/how-to-run-ubuntu-desktop-on-a-virtual-machine-using-virtualbox#1-overview
+# for a guide on how to run Ubuntu inside it.
 sudo apt install -y linux-headers-$(uname -r) dkms
-sudo apt install -y virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso virtualbox-guest-utils
-sudo usermod -a -G vboxusers $USER
+sudo apt install -y virtualbox virtualbox-ext-pack
+sudo usermod -aG vboxusers ${USER}
