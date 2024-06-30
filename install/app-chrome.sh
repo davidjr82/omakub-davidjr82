25a1f36448c3ff2e90f648807271672329e52d9d
David <davidjr82@gmail.com>
2024-06-30 09:34:23 +0200

    Update app-chrome.sh

--- install/app-chrome.sh
+++ install/app-chrome.sh
@@ -1,6 +1,9 @@
-# cd /tmp
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo apt install -y ./google-chrome-stable_current_amd64.deb
-# rm google-chrome-stable_current_amd64.deb
-# xdg-settings set default-web-browser google-chrome.desktop
-# cd -
+mkdir -p /tmp/omakub-davidjr82-debs
+if [ ! -f /tmp/omakub-davidjr82-debs/google-chrome-stable_current_amd64.deb ]; then
+    cd /tmp/omakub-davidjr82-debs
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install -y ./google-chrome-stable_current_amd64.deb
+    # rm google-chrome-stable_current_amd64.deb
+    xdg-settings set default-web-browser google-chrome.desktop
+    cd -
+fi
