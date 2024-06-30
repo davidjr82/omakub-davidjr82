9d8866a6eebc457e5993bd96db9768f51b76569f
David <davidjr82@gmail.com>
2024-06-30 09:37:33 +0200

    Update app-chrome.sh

--- install/app-chrome.sh
+++ install/app-chrome.sh
@@ -1,4 +1,3 @@
-mkdir -p /tmp/omakub-davidjr82-debs
 if [ ! -f /tmp/omakub-davidjr82-debs/google-chrome-stable_current_amd64.deb ]; then
     cd /tmp/omakub-davidjr82-debs
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
