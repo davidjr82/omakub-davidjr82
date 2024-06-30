34f0d1aab8d19100b34ca8f971acb19b465fa858
David Jiménez <djimenez@edatta.com>
2024-06-30 11:43:23 +0200

    wip

--- themes/gnome/nord.sh
+++ themes/gnome/nord.sh
@@ -1,3 +1,3 @@
 OMAKUB_THEME_COLOR="blue"
 OMAKUB_THEME_BACKGROUND="nord_scenary.png"
-source home/$USER/.local/share/omakub-davidjr82/themes/gnome/nord.sh
+source /home/$USER/.local/share/omakub-davidjr82/themes/gnome/nord.sh
