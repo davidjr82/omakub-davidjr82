168ac8545b40d0698f7d038af1f489c7b4f1f5df
David <davidjr82@gmail.com>
2024-07-01 14:16:09 +0200

    Update fonts.sh

--- install/fonts.sh
+++ install/fonts.sh
@@ -3,4 +3,4 @@
 # sudo apt-get install -y ttf-mscorefonts-installer
 
 # downloaded fonts
-for script in /home/djimenez/projects/omakub-davidjr82/fonts/!(_install-font).sh; do source $script; done
+for script in ~/.local/share/omakub-davidjr82/fonts/!(_install-font).sh; do source $script; done
