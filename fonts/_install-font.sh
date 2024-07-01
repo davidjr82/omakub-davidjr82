aebfd59b8139c7a50f69aa9ad9544abb0a27e100
David Jiménez <djimenez@edatta.com>
2024-07-01 12:47:06 +0200

    fonts

--- fonts/_install-font.sh
+++ fonts/_install-font.sh
@@ -4,10 +4,10 @@ FONT_FILE_NAME="${FONT_NAME/ Nerd Font/}"
 
 if ! $(fc-list | grep -i "$FONT_NAME" > /dev/null); then
     cd /tmp/omakub-davidjr82-debs
-    wget -O "$file_name.zip" "$FONT_URL"
-    unzip -o "$file_name.zip" -d "$file_name"
-    cp "$file_name"/*."$FONT_FILE_TYPE" ~/.local/share/fonts
-    rm -rf "$file_name.zip" "$file_name"
+    wget -O "$FONT_FILE_NAME.zip" "$FONT_URL"
+    unzip -o "$FONT_FILE_NAME.zip" -d "$FONT_FILE_NAME"
+    cp "$FONT_FILE_NAME"/*."$FONT_FILE_TYPE" ~/.local/share/fonts
+    rm -rf "$FONT_FILE_NAME.zip" "$FONT_FILE_NAME"
     fc-cache
     cd -
 fi
