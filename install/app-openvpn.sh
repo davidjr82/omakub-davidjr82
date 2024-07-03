89b969acacdb8aff1e1f7ab56ddd17dcbfb48000
David Jiménez <davidjr82@gmail.com>
2024-07-03 12:31:05 +0200

    Update app-openvpn.sh

--- install/app-openvpn.sh
+++ install/app-openvpn.sh
@@ -1,1 +1,11 @@
 sudo apt install -y openvpn
+
+# Add the official openvpn repo
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo wget -qO /etc/apt/keyrings/openvpn.asc https://packages.openvpn.net/packages-repo.gpg
+sudo chmod a+r /etc/apt/keyrings/openvpn.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian $(. /etc/os-release && echo "$VERSION_CODENAME") main" | sudo tee /etc/apt/sources.list.d/openvpn3.list > /dev/null
+sudo apt update
+
+# Install openvpn cli
+sudo apt install openvpn3
