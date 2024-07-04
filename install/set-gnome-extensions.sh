5d95915bfd7ecfed0310ddc447958e6895763fb9
David Jiménez <djimenez@edatta.com>
2024-07-04 15:27:55 +0200

    wip

--- install/set-gnome-extensions.sh
+++ install/set-gnome-extensions.sh
@@ -1,7 +1,5 @@
 sudo apt install -y gnome-shell-extension-manager pipx
 pipx install --force gnome-extensions-cli --system-site-packages
-pipx ensurepath
-source ~/.profile
 
 # Turn off default Ubuntu extensions
 gnome-extensions disable tiling-assistant@ubuntu.com
@@ -10,18 +8,37 @@ gnome-extensions disable ubuntu-dock@ubuntu.com
 gnome-extensions disable ding@rastersoft.com
 
 # Pause to assure user is ready to accept confirmations
-read -p "o install Gnome extensions, you need to accept confirmations. Are you ready?"
+read -p "To install Gnome extensions, you need to accept some confirmations. Are you ready?"
 
 # Install new extensions
+gext install tactile@lundal.io
 gext install just-perfection-desktop@just-perfection
 gext install blur-my-shell@aunetx
 gext install space-bar@luchrioh
+gext install undecorate@sun.wxg@gmail.com
+gext install Vitals@CoreCoding.com
+gext install AlphabeticalAppGrid@stuarthayhurst
+
+sudo apt install -y gir1.2-gda-5.0 gir1.2-gsound-1.0
 
 # Compile gsettings schemas in order to be able to set them
+sudo cp ~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/
 sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas/
 sudo cp ~/.local/share/gnome-shell/extensions/blur-my-shell\@aunetx/schemas/org.gnome.shell.extensions.blur-my-shell.gschema.xml /usr/share/glib-2.0/schemas/
 sudo cp ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/schemas/org.gnome.shell.extensions.space-bar.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/pano\@elhan.io/schemas/org.gnome.shell.extensions.pano.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/Vitals\@CoreCoding.com/schemas/org.gnome.shell.extensions.vitals.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/AlphabeticalAppGrid\@stuarthayhurst/schemas/org.gnome.shell.extensions.AlphabeticalAppGrid.gschema.xml /usr/share/glib-2.0/schemas/
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+# Configure Tactile
+gsettings set org.gnome.shell.extensions.tactile col-0 1
+gsettings set org.gnome.shell.extensions.tactile col-1 2
+gsettings set org.gnome.shell.extensions.tactile col-2 1
+gsettings set org.gnome.shell.extensions.tactile col-3 0
+gsettings set org.gnome.shell.extensions.tactile row-0 1
+gsettings set org.gnome.shell.extensions.tactile row-1 1
+gsettings set org.gnome.shell.extensions.tactile gap-size 32
 
 # Configure Just Perfection
 gsettings set org.gnome.shell.extensions.just-perfection animation 2
@@ -51,5 +68,11 @@ gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-activate-wor
 gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-move-to-workspace-shortcuts true
 gsettings set org.gnome.shell.extensions.space-bar.shortcuts open-menu "@as []"
 
-# Configure tweaks
-gsettings set org.gnome.mutter center-new-windows true
+# Configure Vitals
+gsettings set org.gnome.shell.extensions.vitals hide-icons true
+gsettings set org.gnome.shell.extensions.vitals hot-sensors "['__network-rx_max__']"
+gsettings set org.gnome.shell.extensions.vitals icon-style 0
+gsettings set org.gnome.shell.extensions.vitals network-speed-format 1
+
+# Configure AlphabeticalAppGrid
+gsettings set org.gnome.shell.extensions.alphabetical-app-grid folder-order-position 'end'
